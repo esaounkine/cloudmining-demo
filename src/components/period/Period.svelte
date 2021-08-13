b98c098880e704400eac9b91bc1ef9eb8d189b00
Ilya Saunkin <eli@datamoil.com>
2021-08-13 16:14:56 +0100

    Update the period progress indicator

--- src/components/period/Period.svelte
+++ src/components/period/Period.svelte
@@ -3,57 +3,35 @@ import { VESTING_PERIOD } from "../../state/reducers";
 
   import { state } from "../../state/state";
 
-  const getParticle = (remainder: number) => {
-    console.log(remainder);
-    return remainder <= 10
-      ? "ten"
-      : remainder <= 25
-      ? "twentyfive"
-      : remainder <= 50
-      ? "fifty"
-      : remainder <= 75
-      ? "seventyfive"
-      : "onehundred";
-  };
+  $: progress = Math.round(($state.timeElapsed % VESTING_PERIOD) / VESTING_PERIOD * 100);
 </script>
 
-<pie class={getParticle(($state.timeElapsed % VESTING_PERIOD) / VESTING_PERIOD * 100)} />
+<!-- <pie class={} /> -->
+<div class="outer">
+    <div class="inner" style="width: {progress}%;">
+        <div class="progress">{progress}%</div>
+    </div>
+</div>
 
 <style>
-  pie {
-    width: 25px;
-    height: 25px;
-    display: block;
-    border-radius: 50%;
-    background-color: green;
-    border: 2px solid green;
-    float: left;
-    margin: 1em;
-  }
-
-  .ten {
-    background-image: linear-gradient(126deg, transparent 50%, white 50%),
-      linear-gradient(90deg, white 50%, transparent 50%);
-  }
-
-  .twentyfive {
-    background-image: linear-gradient(180deg, transparent 50%, white 50%),
-      linear-gradient(90deg, white 50%, transparent 50%);
-  }
-
-  .fifty {
-    background-image: linear-gradient(90deg, white 50%, transparent 50%);
-  }
-
-  /* Slices greater than 50% require first gradient
-   to be transparent -> green */
-
-  .seventyfive {
-    background-image: linear-gradient(180deg, transparent 50%, green 50%),
-      linear-gradient(90deg, white 50%, transparent 50%);
-  }
-
-  .onehundred {
-    background-image: none;
-  }
+    .outer {
+        height: 10px;
+        background-color: bisque;
+        position: relative;
+        font-size: 9px;
+        text-align: center;
+    }
+    .inner {
+        position: absolute;
+        width: 0;
+        height: 100%;
+        left: 0;
+        background-color: blue;
+    }
+    .progress {
+        position: absolute;
+        right: 0;
+        color: white;
+        z-index: 100;
+    }
 </style>
